374f4a9377bc849bbb5c9078882bfd97bb3f12d8
Bianca <bikinavisho@yahoo.com>
2017-11-05 17:42:05 -0600

    Enabled Redux in the package.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import initStore from './redux/initStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = initStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
