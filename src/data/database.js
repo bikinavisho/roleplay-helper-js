83b0d4ba79a3e901fa0b26bdf3195f67f146d7cd
Bianca <bikinavisho@users.noreply.github.com>
2018-06-29 18:22:24 -0500

    Remove sensitive data from source control

--- src/data/database.js
+++ src/data/database.js
@@ -1,13 +1,13 @@
-import Firebase from 'firebase';
+import firebase from 'firebase';
 
 // Initialize Firebase
 let config = {
-  apiKey: "AIzaSyByBXGFBJApMnEQnrUr4okDO3csuA6Vj2M",
-  authDomain: "roleplay-helper.firebaseapp.com",
-  databaseURL: "https://roleplay-helper.firebaseio.com",
-  projectId: "roleplay-helper",
-  storageBucket: "",
-  messagingSenderId: "1049013151879"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSENGING_SENDER_ID
 };
 firebase.initializeApp(config);
 const database = firebase.database();
