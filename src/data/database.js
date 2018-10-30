85f309ec5661a754433b9393401a1e86881ba629
Bianca <bikinavisho@yahoo.com>
2018-10-29 20:17:55 -0500

    update environment variables to have prefix REACT_APP

--- src/data/database.js
+++ src/data/database.js
@@ -4,18 +4,14 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 let config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSENGING_SENDER_ID
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGING_SENDER_ID
 };
 
-console.log('NONSTRING ', process.env.REACT_APP_NONSTRING)
-console.log('STRING ', process.env.REACT_APP_STRING)
-
-console.log('config ', config);
 firebase.initializeApp(config);
 const database = firebase.database();
 
