538f1a2279ca7f0c829d6ab147df0e58796362b1
Bianca <bikinavisho@yahoo.com>
2018-11-15 18:36:01 -0600

    starters for application menu

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import CharacterSheet from "./components/CharacterSheet/";
 class App extends Component {
 	render() {
 		return (
+			<React.Fragment>
+				<div>HEADER??</div>
 			<Switch>
 				<Route exact path='/' component={FindCharacter}/>
 				<Route path='/character' component={CharacterSheet}/>
 			</Switch>
+			</React.Fragment>
 		);
 	}
 }
